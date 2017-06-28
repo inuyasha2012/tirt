db51f9c1321b852f4e097b50ace476beef1b94d1
inuyasha2012 <chinachrist@163.com>
2017-06-28 22:36:48 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='tirt',
+    version='0.0.1',
+    packages=['tirt'],
+    url='https://github.com/inuyasha2012/tirt',
+    license='MIT',
+    author='inuyasha2012',
+    author_email='inuyasha021@163.com',
+    description='the simulation of Thurstone Item Response Theory, include fixed forced test and adaptive forced test. ',
+    install_requires=['numpy', 'scipy']
+)

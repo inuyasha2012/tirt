2ab974860fbbd8991511d00299ccce01b1c2aecb
inuyasha2012 <chinachrist@163.com>
2017-06-29 12:18:08 +0800

    add readme content

--- setup.py
+++ setup.py
@@ -2,12 +2,16 @@ from setuptools import setup
 
 setup(
     name='tirt',
-    version='0.0.1',
+    version='0.0.3',
     packages=['tirt'],
     url='https://github.com/inuyasha2012/tirt',
     license='MIT',
     author='inuyasha2012',
     author_email='inuyasha021@163.com',
     description='the simulation of Thurstone Item Response Theory, include fixed forced test and adaptive forced test. ',
-    install_requires=['numpy', 'scipy']
+    long_description=open('doc/index.rst').read(),
+    install_requires=['numpy', 'scipy'],
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+    ]
 )
